5c14e5c0a36968dfcf498a0236583cb7670f4425
pitfigu <pitcarepa@gmail.com>
2026-04-01 17:08:56 +0200

    fix: handle missing blogs directory in mdx.ts for Vercel build

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -12,6 +12,7 @@ type Metadata = {
 };
 
 function getMDXFiles(dir: string) {
+  if (!fs.existsSync(dir)) return [];
   return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
 }
 
